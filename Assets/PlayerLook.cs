b004dc46036920cd598a086c36ebf16d37300a5c
Vinicius <vdreifke@inf.ufsm.br>
2021-03-27 19:11:33 -0300

    movimentação e camera do player

---
+++ Assets/PlayerLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    private float mouseX;
+    private float mouseY;
+    private float mouseSensitivity;
+
+    public Transform playerBody;
+
+    private float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRotation = 0f;
+        mouseSensitivity = 900f;
+        //Lock mouse
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+
+
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float x;
+    private float z;
+    private float speed;
+    private Vector3 move;
+
+    public CharacterController controller;
+
+    private void Start()
+    {
+        speed = 5f;
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
