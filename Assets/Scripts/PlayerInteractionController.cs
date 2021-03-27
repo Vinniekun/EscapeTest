eb0b19c21187eb09f6b005c815b7a86e4bba9b27
Vinicius <vdreifke@inf.ufsm.br>
2021-03-27 20:45:43 -0300

    interacao com objetos

---
+++ Assets/Scripts/PlayerInteractionController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteractionController : MonoBehaviour
+{
+    private float maxDistance;
+    public LayerMask interactableLayers;
+    public Button interactButton;
+    public Interactable currentInteractable;
+    public GameObject interactableObject;
+
+    void Start()
+    {
+        maxDistance = 2;
+        interactableObject = null;
+    }
+
+    
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxDistance, interactableLayers))
+        {
+            currentInteractable = hit.collider.GetComponent<Interactable>();
+            interactableObject = hit.collider.gameObject;
+        }
+        else
+        {
+            currentInteractable = null;
+            interactableObject = null;
+        }
+        //interactButton.interactable = currentInteractable != null;
+
+        if (currentInteractable)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Interact();
+            }
+        }
+
+    }
+
+    public void Interact()
+    {
+        if (currentInteractable)
+        {
+            currentInteractable.OnInteraction();
+        }
+    }
+
+}
